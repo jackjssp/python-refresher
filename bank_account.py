9955edfb3e3c3c6dcb0f5347b8e54a0aba42dd62
jackjssp <jackjssp0@gmail.com>
2026-07-08 14:44:35 -0400

    added linalg unit

--- bank_account.py
+++ bank_account.py
@@ -3,17 +3,24 @@ class BankAccount:
         self.balance = balance
         self.name = name
         self.number = number
+
     def withdraw(self, amountToWithdraw):
-        if (amountToWithdraw > self.balance):
+        self.transaction(-amountToWithdraw)
+
+    def deposit(self, amountToDeposit):
+        self.transaction(amountToDeposit)
+
+    def transaction(self, amount):
+        if (self.balance + amount < 0):
             print("You cannot withdraw more than you have.")
         else:
-            self.balance -= amountToWithdraw
-    def deposit(self, amountToDeposit):
-        self.balance += amountToDeposit
+            self.balance += amount
+
     def printBalance(self):
-        print(f"Your balance is: {self.balance}")
+        print(f"The balance of account {self.number} is: {self.balance}")
 
 p1 = BankAccount(10000, "Jack", 293)
 p1.deposit(2000)
+p1.withdraw(13000)
 p1.withdraw(3000)
 p1.printBalance()
---
+++ linear_algebra.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def sumDiff(a, b):
+    sum = a + b
+    diff = a - b
+    print(f"\na = \n{a}")
+    print(f"\nb = \n{b}")
+    print(f"\nsum = \n{sum}")
+    print(f"\ndifference = \n{diff}")
+
+#Problem 1
+print("Problem 1:")
+a = np.array([[1], [2], [3]])
+b = np.array([[4], [5], [6]])
+sumDiff(a, b)
+
+#Problem 2
+print("\n\nProblem 2:")
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+sum = a + b
+diff = a - b
+print(f"\na = \n{a}")
+print(f"\nb = \n{b}")
+print(f"\nsum = \n{sum}")
+print(f"\ndifference = \n{diff}")
+
+#Problem 3
+print("\n\nProblem 3:")
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+dotProduct = np.dot(a, b)
+print(f"\na = {a}")
+print(f"\nb = {b}")
+print(f"\nDot product = {dotProduct}")
+
+#Problem 4
+print("\n\nProblem 4:")
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]])
+product = np.dot(a, b)
+print(f"\na = \n{a}")
+print(f"\nb = \n{b}")
+print(f"\nproduct = \n{product}")
+
+#Problem 5
+print("\n\nProblem 5:")
+a = np.array([1, 1, 2])
+magnitude = np.linalg.norm(a)
+print(f"\na = {a}")
+print(f"\nmagnitude = {magnitude}")
+
+#Problem 6
+print("\n\nProblem 6:")
+a = np.array([[1, 3], [2, 4]])
+transpose = np.transpose(a)
+print(f"\na = \n{a}")
+print(f"\ntranspose = \n{transpose}")
